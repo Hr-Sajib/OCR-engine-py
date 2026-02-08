845c845c2b422831200ecac199d734599426d500
Hr-Sajib <hrsajib001@gmail.com>
2026-02-08 12:17:37 +0600

    port fix

--- main.py
+++ main.py
@@ -102,4 +102,4 @@ async def extract(files: List[UploadFile] = File(...)):
 # --- Run standalone ---
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8100, reload=True)
